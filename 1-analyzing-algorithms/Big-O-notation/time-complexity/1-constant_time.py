561f7bac103ab57b02cc38f61e7dbc95229c8b67
unknown <berkozzambak@gmail.com>
2021-04-01 23:02:46 +0200

    initial commit with algorithm analyses

---
+++ 1-analyzing-algorithms/Big-O-notation/time-complexity/1-constant_time.py
@@ -0,0 +1,8 @@
+def get_middle_value(arr):
+    return arr[len(arr)//2]
+
+    '''
+    This simple function has a constant run time complexity, O(1)
+    No matter how big the array size is, the method will have the same
+    amount of calculations to execute.
+    '''
---
+++ 1-analyzing-algorithms/Big-O-notation/time-complexity/2-linear_time.py
@@ -0,0 +1,42 @@
+def simple_search(arr, target):
+    '''
+    Simple or linear search is an example of linear run time complexity.
+
+    GOAL
+        The goal of the algorithm is to find the target value's index in the provided array.
+
+    INPUTS:
+        The algorithm accepts two inputs; an array and a target value to be found in the array.
+
+    TERMINATION CONDITIONS:
+        The algorithm terminates/returns in three conditions:
+        1) If the provided array is empty
+        2) If the target value is found
+        3) If end of the array is reached without finding the target value
+
+    STEPS
+        The search starts from the first element of the array. If that is equal to the target value,
+        the current index is returned. If not, the algorithm moves on to the next index. This comparison
+        continues until the two values match (case a) or the end of the array is reached (case b).
+        When case a:
+            - The index is returned and the function terminates
+        When case b:
+            - 'Not found' string is returned and the function terminates
+
+    TIME COMPLEXITY
+        1) Best Case:
+            If the target value is located in the very beginning of the array, the search takes only 
+            one step to complete. Therefore it would be constant time. (O(1))
+
+        2)Worst Case:
+            The number of steps/calculations increases if the target value is located in the end of the
+            array or if the array does not include the target value. In these cases, in order to terminate,
+            the algorithm has to make as many operations as the size of the array. 
+            Therefore, the worst case time complexity is dependant on the array size and has a linear time complexity,
+            O(n)
+
+        3)Average Case:
+            In the average case, the target value is located close to the middle of the array. Depending on the array size and
+            if the array is sorted, linear search can be an acceptable/rational algorithm choice.  
+
+    '''
