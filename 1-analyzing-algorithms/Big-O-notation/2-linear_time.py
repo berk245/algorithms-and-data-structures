baa21fe2552b735d2ca933f5c2a1357ffcbf776f
unknown <berkozzambak@gmail.com>
2021-04-02 14:41:28 +0200

    initial commit to dev branch

---
+++ 1-analyzing-algorithms/Big-O-notation/2-linear_time.py
@@ -0,0 +1,10 @@
+def print_each(arr):
+    for value in arr:
+        print(value)
+
+
+'''
+    Linear Time Complexity describes an algorithm or program who’s complexity will grow in direct proportion to the size of the input data. In the above simple example,
+    the number of operations are directly linked to the array size since it goes through. 
+    
+'''
--- 1-analyzing-algorithms/Big-O-notation/5-exponential_time.py
+++ 1-analyzing-algorithms/Big-O-notation/5-exponential_time.py
@@ -2,4 +2,6 @@
 Exponential Time complexity refers to an algorithm whose growth doubles with each additon to the input data set.
 
 This implementation of Fibonacci series function is a good example, although this is not the only way to implement it.
+
+This section will be updated
 '''
