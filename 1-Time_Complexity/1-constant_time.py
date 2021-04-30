db5f57ec93575129131474cbb76feb2061723ffb
unknown <berkozzambak@gmail.com>
2021-04-30 20:12:39 +0200

    attempting to fix file structure bug

--- 1-Time_Complexity/1-constant_time.py
+++ 1-Time_Complexity/1-constant_time.py
@@ -1,7 +1,8 @@
 def get_middle_value(arr):
     return arr[len(arr)//2]
 
-    '''
+
+'''
     Constant Time Complexity describes an algorithm whose execution takes the same time regardless of the size of the input data set.
     This simple function has a constant run time complexity, O(1)
     No matter how big the array size is, the method will have the same
